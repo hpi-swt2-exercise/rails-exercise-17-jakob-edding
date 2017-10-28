93738525cbc18029b2cc5026b360487dd105ccf1
--global <lupin2702@gmail.com>
2017-10-29 01:08:09 +0200

    Add authors controller, set route, add new action. Closes #1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  # get 'authors/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+  resources :authors
   # You can have the root of your site routed with "root"
   root 'home#index'
- 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
