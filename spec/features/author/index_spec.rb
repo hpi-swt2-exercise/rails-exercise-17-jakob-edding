4a4da8aa1d09d0b8230ea597b326a0b62ed4d3db
Jakob Edding <lupin2702@gmail.com>
2017-10-29 19:14:31 +0100

    Failing test for #8

--- spec/features/author/index_spec.rb
+++ spec/features/author/index_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 describe "Author index page", type: :feature do
 
- it "should render withour error" do
-   visit authors_path
- end
+    it "should render withour error" do
+        visit authors_path
+    end
 
+    it "should show author Alan Turing after he's been entered" do
+        visit new_author_path
+        fill_in "author_first_name", :with => "Alan"
+        fill_in "author_last_name", :with => "Turing"
+        fill_in "author_homepage", :with => "http://wikipedia.de/Alan_Turing"
+        find('input[type="submit"]').click
+
+        visit authors_path
+        expect(page).to have_content('Alan Turing')
+    end
 end
