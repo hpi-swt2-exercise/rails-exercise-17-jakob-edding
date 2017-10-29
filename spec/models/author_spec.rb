26bd447a3dea7618a6c01c25381c5d689b248e1f
--global <lupin2702@gmail.com>
2017-10-29 16:42:54 +0100

    Failing test for #4

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "Author model", type: :model do
+
+    it "Should have properties first name, last name, homepage" do
+         author = Author.new({
+             "first_name"=>"Alan",
+             "last_name"=>"Turing",
+             "homepage"=>"turing.com"
+         })
+         expect(author.first_name).to eq('Alan')
+         expect(author.last_name).to eq('Turing')
+         expect(author.homepage).to eq('turing.com')
+     end
+
+end
