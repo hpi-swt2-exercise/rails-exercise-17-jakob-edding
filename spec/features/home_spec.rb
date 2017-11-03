7182a31b8a12cd67ae17798b8c59c6495001984b
Jakob Edding <lupin2702@gmail.com>
2017-11-03 23:25:08 +0100

    Failing test for links to author/paper overview from homepage

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe "Index page", :type => :feature do
-  
+
   it "should display 'Paper Management System'" do
     visit root_path
     expect(page).to have_text("Paper Management System")
+  end
+
+  it "should have a link to paper index" do
+    visit root_path
+    expect(page).to have_css('a', :text => "Show papers")
+  end
+
+  it "should have a link to author index" do
+    visit root_path
+    expect(page).to have_css('a', :text => "Show authors")
   end
 end
