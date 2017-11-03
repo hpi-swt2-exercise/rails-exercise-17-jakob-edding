4ff3cd4ac67437b0d92b662e367f746b21d0c364
Jakob Edding <lupin2702@gmail.com>
2017-11-03 14:32:33 +0100

    Add edit author action and view. Closes #14

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,6 +9,10 @@ class AuthorsController < ApplicationController
         end
     end
 
+    def edit
+      @author = Author.find(params[:id])
+    end
+
     def index
         @authors = Author.all()
     end
