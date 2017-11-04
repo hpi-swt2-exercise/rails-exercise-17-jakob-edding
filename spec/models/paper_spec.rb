8dc7b1475829390dcd51890e03cd0e32e57fd590
Jakob Edding <lupin2702@gmail.com>
2017-11-04 23:00:36 +0100

    Failing test for #20

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should not be allowed to have an empty title" do
+    @paper = Paper.create(title: nil, venue: "Someplace", year: 9000)
+    expect(@paper.valid?).to eq(false)
+  end
 end
