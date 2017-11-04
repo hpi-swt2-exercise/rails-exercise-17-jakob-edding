371e18e2eddd38b8e89bf18b33475e5947f8aed9
Jakob Edding <lupin2702@gmail.com>
2017-11-04 23:22:53 +0100

    Failing test for #24

--- spec/features/paper/index_spec.rb
+++ spec/features/paper/index_spec.rb
@@ -7,4 +7,9 @@ describe "Paper index page", type: :feature do
         expect(page).to have_css('a', :text => "Back")
     end
 
+    it "should have a link to add a new paper" do
+        visit papers_path
+        expect(page).to have_css('a', :text => "Add paper")
+    end
+
 end
