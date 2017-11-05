79882e51bb82ff90ee6736771020873de272353d
Jakob Edding <lupin2702@gmail.com>
2017-11-05 19:02:59 +0100

    Failing test for #31

--- spec/features/paper/index_spec.rb
+++ spec/features/paper/index_spec.rb
@@ -12,4 +12,11 @@ describe "Paper index page", type: :feature do
         expect(page).to have_css('a', :text => "Add paper")
     end
 
+    it "should filter by year if user visits page with parameter 'year' set" do
+      @old_paper = FactoryGirl.create :paper, year: 1950
+      @new_paper = FactoryGirl.create :paper, year: 2000
+
+      visit papers_path(year: @old_paper.year)
+      expect(page).to have_no_content(@new_paper.year)
+    end
 end
