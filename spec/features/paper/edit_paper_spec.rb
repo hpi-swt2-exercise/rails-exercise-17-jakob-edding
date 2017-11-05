db9b492cf14f8f4613b60dbb538ab08e8460ac00
Jakob Edding <lupin2702@gmail.com>
2017-11-05 11:23:05 +0100

    Failing test for #28

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -9,4 +9,17 @@ describe "Edit paper page", type: :feature do
     expect(page).to have_field('Author 1')
   end
 
+  it "should be allowed to add a new author to a paper" do
+    @alan = FactoryGirl.create :author
+    @paper = FactoryGirl.create :paper, authors: [@alan]
+
+    @plagiarist = FactoryGirl.create :author, first_name: 'Peter', last_name: 'Plagiarist'
+
+    visit edit_paper_path(@paper)
+    page.select @plagiarist.name, :from => 'paper_author_id_1'
+    find('input[type="submit"]').click
+
+    expect(page).to have_content(@plagiarist.name)
+  end
+
 end
